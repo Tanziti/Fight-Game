ce208aad5a89d8a67f0a7b6d99cd93785a49311b
Zi Tan <tanzitian3@gmail.com>
2024-02-09 15:19:10 -0500

    shop animation slowed

--- js/classes.js
+++ js/classes.js
@@ -26,11 +26,15 @@ class Sprite {
 
     update() {
         this.draw()
+        this.framesElapsed++
+
+        if (this.framesElapsed % this.framesHold === 0){
         if (this.framesCurrent < this.framesMax - 1){
         this.framesCurrent++;
         } else {
             this.framesCurrent = 0;
         }
+      }
     }
 }
 
