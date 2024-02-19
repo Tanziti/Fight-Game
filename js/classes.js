e6271820e9982c8d632420a7166ebe17e33f2201
Zi Tan <tanzitian3@gmail.com>
2024-02-19 13:20:07 -0500

    attack1 finished, a bit slow?

--- js/classes.js
+++ js/classes.js
@@ -109,6 +109,7 @@ class Fighter extends Sprite{
 
     }
     attack(){
+        this.switchSprite('attack1') 
         this.isAttacking = true
         setTimeout(() => {
         this.isAttacking = false
@@ -116,6 +117,8 @@ class Fighter extends Sprite{
     }
 
     switchSprite(sprite) {
+        if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) return 
+
         switch(sprite) {
             case 'idle':
                 if (this.image !== this.sprites.idle.image){
@@ -142,6 +145,13 @@ class Fighter extends Sprite{
                 if (this.image !== this.sprites.fall.image){
                 this.image = this.sprites.fall.image
                 this.framesMax = this.sprites.fall.framesMax
+                this.framesCurrent = 0
+                } 
+                break
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image){
+                this.image = this.sprites.attack1.image
+                this.framesMax = this.sprites.attack1.framesMax
                 this.framesCurrent = 0
                 } 
                 break
