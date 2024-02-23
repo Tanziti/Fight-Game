c7095d033c2a83845636310e49c98d09c3f712bd
Zi Tan <tanzitian3@gmail.com>
2024-02-22 22:01:56 -0500

    recreate attack box

--- js/classes.js
+++ js/classes.js
@@ -99,6 +99,8 @@ class Fighter extends Sprite{
         
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
+
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
        
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
